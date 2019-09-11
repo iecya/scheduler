0e310b2c8d3e200b785d3cc3e9960cbd30bc4cce
iecya <federica.delisi@hotmail.it>
2019-09-11 17:42:59 +0100

    Validate line with failing tests

--- src/scheduler/core.clj
+++ src/scheduler/core.clj
@@ -1,6 +1,15 @@
 (ns scheduler.core
-  (:require [scheduler.utils :as utils])
+  (:require [scheduler.utils :as utils]
+            [clojure.pprint :as pprint]
+            [clojure.java.io :as io])
   (:gen-class))
+
+
+(defn process-line
+  [line]
+  (if (= "exit" line)
+    (System/exit 0)
+    (println line)))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -8,8 +17,9 @@
   ; validate time input - not nil and format HH:MM
   (let [validated-time (utils/validate-time-input time)]
     (if (nil? validated-time)
-      (.write *out* "Please input a valid time in HH:MM format")
-      (.write *out* (str "Time is " time))))
-  ; read input
-  )
+      (println "Please input a valid time in HH:MM format")
 
+      ; read input
+      (with-open [reader (io/reader *in*)]
+        (println "Please insert your input:")
+        (doall (map process-line (line-seq reader)))))))
--- src/scheduler/utils.clj
+++ src/scheduler/utils.clj
@@ -3,3 +3,7 @@
 (defn validate-time-input
   [input]
   (when (seq input) (re-matches #"(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]" input)))
+
+(defn validate-line
+  [line]
+  (when (seq line) (re-matches #"([0-5][0-9]|\*)\s([0-1][0-9]|2[0-3]|\*)\s/bin/(\w+)" line)))
