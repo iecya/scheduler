fd32224e656f089925738854a797858b2118cf58
iecya <federica.delisi@hotmail.it>
2019-09-11 13:38:31 +0100

    Improve test and fix regex

--- src/scheduler/utils.clj
+++ src/scheduler/utils.clj
@@ -2,4 +2,4 @@
 
 (defn validate-time-input
   [input]
-  (when (seq input) (re-matches #"/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/" input)))
+  (when (seq input) (re-matches #"(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]" input)))
--- test/scheduler/core_test.clj
+++ test/scheduler/core_test.clj
@@ -5,4 +5,7 @@
 
 (deftest time-input-test
   (testing "Given a time input, verifies it is in the valid format"
-    (is (utils/validate-time-input "24:50"))))
+    (is (nil? (utils/validate-time-input "24:50")))
+    (is (nil? (utils/validate-time-input "03:60")))
+    (is (utils/validate-time-input "11:35"))
+    (is (utils/validate-time-input "05:59"))))
